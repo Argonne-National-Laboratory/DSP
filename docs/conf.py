4c47f81a2dd5f64310e07c0a089c2ee3c93b99c9
Kibaek Kim <kibaek.k@gmail.com>
2015-11-30 14:36:03 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -226,7 +226,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'DSP.tex', u'DSP Documentation',
-   u'Kibaek Kim', 'manual'),
+   u'Kibaek Kim and Victor M. Zavala', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
