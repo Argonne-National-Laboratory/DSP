73070c498cb8179094e87f66b7e2d14e84d54a49
Kibaek Kim <kimk@anl.gov>
2016-08-12 00:19:19 -0500

    update

--- examples/farmer/farmer_model.jl
+++ examples/farmer/farmer_model.jl
@@ -1,46 +1,44 @@
-# Kibaek Kim - ANL MCS 2014
+# Kibaek Kim - ANL MCS 2016
 # Farmer example from Birge and Louveaux book.
 
+using JuMP, Dsp
 
-# CREATE STOCHASTIC MODEL
+NS = 3;                        # number of scenarios
+probability = [1/3, 1/3, 1/3]; # probability
 
-m = StochasticModel(NS);
+CROPS  = 1:3 # set of crops (wheat, corn and sugar beets, resp.)
+PURCH  = 1:2 # set of crops to purchase (wheat and corn, resp.)
+SELL   = 1:4 # set of crops to sell (wheat, corn, sugar beets under 6K and those over 6K)
 
+Cost     = [150 230 260]   # cost of planting crops
+Budget   = 500             # budget capacity
+Purchase = [238 210];      # purchase price
+Sell     = [170 150 36 10] # selling price
+Yield    = [3.0 3.6 24.0;
+            2.5 3.0 20.0;
+            2.0 2.4 16.0]
+Minreq   = [200 240 0]     # minimum crop requirement
 
-# FIRST-STAGE MODEL
+# JuMP model
+# m = Model(solver = DspSolver(method = :DD, param = "myparams.txt"))
+m = Model()
 
-# first-stage variables
-@defVar(m, x[i=CROPS] >= 0, Int)
+@variable(m, x[i=CROPS] >= 0, Int)
+@objective(m, Min, sum{Cost[i] * x[i], i=CROPS})
+@constraint(m, const_budget, sum{x[i], i=CROPS} <= Budget)
 
-# first-stage objective
-@setObjective(m, Min, sum{Cost[i] * x[i], i=CROPS})
+for s in 1:NS
+    blk = Model()
 
-# first-stage constraint
-@addConstraint(m, const_budget,
-               sum{x[i], i=CROPS} <= Budget)
+    @variable(blk, y[j=PURCH] >= 0)
+    @variable(blk, w[k=SELL] >= 0)
 
+    @objective(blk, Min, sum{Purchase[j] * y[j], j=PURCH} - sum{Sell[k] * w[k], k=SELL})
 
-# SECOND-STAGE MODELS
+    @constraint(blk, const_minreq[j=PURCH], Yield[s,j] * x[j] + y[j] - w[j] >= Minreq[j])
+    @constraint(blk, const_minreq_beets, Yield[s,3] * x[3] - w[3] - w[4] >= Minreq[3])
+    @constraint(blk, const_aux, w[3] <= 6000)
 
-@second_stage m s begin
-    # stochastic block
-    sb = StochasticBlock(m, probability[s]);
-
-    # second-stage variables
-    @defVar(sb, y[j=PURCH] >= 0)
-    @defVar(sb, w[k=SELL] >= 0)
-
-    # objective
-    @setObjective(sb, Min,
-                  sum{Purchase[s,j] * y[j], j=PURCH}
-                  - sum{Sell[s,k] * w[k], k=SELL})
-
-    # constraints
-    @addConstraint(sb, const_minreq[j=PURCH],
-                   Yield[s,j] * x[j] + y[j] - w[j] >= Minreq[s,j])
-    @addConstraint(sb, const_minreq_beets,
-                   Yield[s,3] * x[3] - w[3] - w[4] >= Minreq[s,3])
-    @addConstraint(sb, const_aux, w[3] <= 6000)
+    # add blk to m as a block
+    @block(m, blk, s, probability[s])
 end
-
-
--- examples/farmer/farmer_run_mpi.jl
+++ examples/farmer/farmer_run_mpi.jl
@@ -2,39 +2,18 @@
 # Kibaek Kim - ANL MCS 2014
 # Farmer example from Birge and Louveaux book.
 
-using MPI, StochJuMP, DSPsolver
+using MPI
 
+# initialize
 MPI.Init();
-
-# data file
-include("farmer_data.jl")
 
 # model file
 include("farmer_model.jl")
 
-# load problem to model object
-DSPsolver.loadProblem(m);
+# Dsp solve types
+solve_types = [:Dual, :Benders, :Extensive]
 
-# set parameters
-DSPsolver.setIntParam("LOG_LEVEL",3);
-DSPsolver.setIntParam("DD/ITER_LIM",100);
-DSPsolver.setDblParam("SCIP/GAP_TOL",0.0);
+solve(m, solve_type = solve_types[1])
 
-init_solution = [170.0,79.99999999999997,250.00000000000003,0.0,0.0,310.0,47.999999999999886,6000.0,9.094947017729282e-13,0.0,0.0,225.0,-8.526512829121202e-14,5000.000000000001,0.0,0.0,48.000000000000085,140.0,0.0,4000.0000000000005,0.0];
-DSPsolver.setSolution(init_solution);
-
-# solve problem
-DSPsolver.solve(DSP_SOLVER_BD_MPI);
-#DSPsolver.solveBdMpi();
-
-if MPI.Comm_rank(MPI.COMM_WORLD) == 0
-	# print some results
-	println("Solution Status: ", DSPsolver.getSolutionStatus());
-	println("Primal Bound   : ", DSPsolver.getPrimalBound());
-	println("Dual Bound     : ", DSPsolver.getDualBound());
-
-	print(DSPsolver.getSolution());
-end
-
-MPI.Finalize();
-
+# finalize
+MPI.Finalize()
