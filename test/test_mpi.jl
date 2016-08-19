6e6994c7d51bafa0b139382e2e2de0611304b64f
Kibaek Kim <kimk@anl.gov>
2016-08-19 12:05:38 -0500

    update

--- test/test_mpi.jl
+++ test/test_mpi.jl
@@ -3,6 +3,13 @@
 
 using MPI, Dsp
 
+test_name    = AbstractString[]
+test_method  = Symbol[]
+test_status  = Symbol[]
+test_primobj = Float64[]
+test_dualobj = Float64[]
+test_time    = Float64[]
+
 MPI.Init()
 
 # parameter file
@@ -18,14 +25,22 @@ include("../examples/farmer/farmer_model.jl")
 solve_types = [:Dual, :Benders]
 
 for st in solve_types
-    println("\n################################################################################")
-    println("# Solve farmer using ", st)
-    println("################################################################################\n")
-    solve(m, solve_type = st, param = myparam, comm = MPI.COMM_WORLD)
+    if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+        println("\n################################################################################")
+        println("# Solve farmer using ", st)
+        println("################################################################################\n")
+    end
+    status = solve(m, solve_type = st, param = myparam, comm = MPI.COMM_WORLD)
     if MPI.Comm_rank(MPI.COMM_WORLD) == 0
         @show getprimobjval()
         @show getdualobjval()
         @show getsolutiontime()
+        push!(test_name, "farmer(JuMP)")
+        push!(test_method, st)
+        push!(test_status, status)
+        push!(test_primobj, getprimobjval())
+        push!(test_dualobj, getdualobjval())
+        push!(test_time, getsolutiontime())
     end
 end
 
@@ -36,16 +51,31 @@ for f in smps_files
         fname = joinpath(smps_dir, f[1:pos])
         readSmps(fname)
         for st in solve_types
-            println("\n################################################################################")
-            println("# Solve $fname using ", st)
-            println("################################################################################\n")
-            optimize(solve_type = st, param = myparam, comm = MPI.COMM_WORLD)
+            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+                println("\n################################################################################")
+                println("# Solve $fname using ", st)
+                println("################################################################################\n")
+            end
+            status = optimize(solve_type = st, param = myparam, comm = MPI.COMM_WORLD)
             if MPI.Comm_rank(MPI.COMM_WORLD) == 0
                 @show getprimobjval()
                 @show getdualobjval()
                 @show getsolutiontime()
+                push!(test_name, f[1:pos])
+                push!(test_method, st)
+                push!(test_status, status)
+                push!(test_primobj, getprimobjval())
+                push!(test_dualobj, getdualobjval())
+                push!(test_time, getsolutiontime())
             end
         end
+    end
+end
+
+if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+    for i in 1:length(test_name)
+        @printf("%15s  %10s  %15s  %+10e  %+10e  %6.2f\n", 
+            test_name[i], test_method[i], test_status[i], test_primobj[i], test_dualobj[i], test_time[i])
     end
 end
 
