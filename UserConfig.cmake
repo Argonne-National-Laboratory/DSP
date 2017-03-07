340b800ffcdde77de062dad81d823da07f918f2a
Kibaek Kim <kibaek.k@gmail.com>
2017-03-07 13:41:56 -0600

    added submodules

---
+++ UserConfig.cmake
@@ -0,0 +1,13 @@
+# This file defines the user-specific settings.
+
+set(MA27LIB_DIR   "")
+set(CPLEX_LIB_DIR "")
+set(CPLEX_INC_DIR "")
+set(DEPEND_DIR    $ENV{PWD})
+
+# Please change OFF to ON once the settings are provided.
+set(USER_SETTINGS OFF)
+
+if(NOT ${USER_SETTINGS})
+	message(FATAL_ERROR "Please complete the user-specific settings in UserConfig.cmake")
+endif()
