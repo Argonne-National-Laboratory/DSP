3adb7a1be3a304a9f7988b911d5c54e8a56bdd45
Kibaek Kim <kibaek.k@gmail.com>
2018-05-25 22:36:04 -0500

    refactored code

--- src/Solver/DantzigWolfe/DwBranchNonant.cpp
+++ src/Solver/DantzigWolfe/DwBranchNonant.cpp
@@ -5,73 +5,42 @@
  *      Author: Kibaek Kim
  */
 
-//#define DSP_DEBUG
+// #define DSP_DEBUG
 #include <DantzigWolfe/DwBranchNonant.h>
-#include <Model/TssModel.h>
+
+DwBranchNonant::DwBranchNonant(DwModel* model) : DwBranch(model) {
+	DecSolver* solver = model_->getSolver();
+	master_ = dynamic_cast<DwMaster*>(solver);
+	if (solver->getModelPtr()->isStochastic())
+		tss_ = dynamic_cast<TssModel*>(solver->getModelPtr());
+}
 
 bool DwBranchNonant::chooseBranchingObjects(
 		std::vector<DspBranchObj*>& branchingObjs /**< [out] branching objects */) {
 
-	DspMessage* message = model_->getSolver()->getMessagePtr();
+	if (tss_ == NULL) return false;
 
 	bool branched = false;
 	int branchingIndex = -1;
 	double branchingValue, branchingDownValue, branchingUpValue;
 
-	DecSolver* solver = model_->getSolver();
-	DwMaster* master = dynamic_cast<DwMaster*>(solver);
-	int ncols_first_stage;
 	std::vector<double> refsol; // reference solution
 	std::vector<double> devsol; // devations from the refsol
 	std::vector<std::vector<double>> densesol; // dense solution vector
-
-	/** two-stage stochastic model */
-	TssModel* tss = NULL;
-	if (solver->getModelPtr()->isStochastic()) {
-		tss = dynamic_cast<TssModel*>(solver->getModelPtr());
-		ncols_first_stage = tss->getNumCols(0) * tss->getNumScenarios();
-	} else {
-		return branched;
-	}
-
-	if (master->getLastSubprobSolutions().size() != tss->getNumScenarios()) {
-		message->print(0, "The number of subproblem solutions is not equal to the number of scenarios.\n");
-		return branched;
-	}
 
 	DspBranchObj* branchingUp = NULL;
 	DspBranchObj* branchingDn = NULL;
 
 	BGN_TRY_CATCH
 
+	/** retreive message object */
+	DspMessage* message = model_->getSolver()->getMessagePtr();
+
 	/** calculate reference solution */
-	refsol.resize(tss->getNumCols(0), 0.0);
-	densesol.reserve(master->getLastSubprobSolutions().size());
-	for (unsigned i = 0; i < master->getLastSubprobSolutions().size(); ++i) {
-		CoinPackedVector* sol = master->getLastSubprobSolutions()[i];
-#ifdef DSP_DEBUG2
-		printf("Subproblem solution (scenario %d):\n", i);
-		DspMessage::printArray(sol);
-#endif
-		std::vector<double> dsol(tss->getNumCols(0), 0.0);
-		for (int j = 0; j < sol->getNumElements(); ++j) {
-			if (sol->getIndices()[j] < ncols_first_stage) {
-				refsol[sol->getIndices()[j] % tss->getNumCols(0)] += sol->getElements()[j] * tss->getProbability()[i];
-				dsol[sol->getIndices()[j] % tss->getNumCols(0)] += sol->getElements()[j];
-			}
-		}
-		densesol.push_back(dsol);
-		sol = NULL;
-	}
+	getRefSol(refsol);
 
 	/** calculate variances from the reference solution */
-	devsol.resize(tss->getNumCols(0), 0.0);
-	for (unsigned i = 0; i < master->getLastSubprobSolutions().size(); ++i) {
-		CoinPackedVector* sol = master->getLastSubprobSolutions()[i];
-		for (int j = 0; j < tss->getNumCols(0); ++j)
-			devsol[j] += pow(densesol[i][j] - refsol[j],2) * tss->getProbability()[i];
-		sol = NULL;
-	}
+	getDevSol(refsol, devsol);
 
 #ifdef DSP_DEBUG
 	printf("Reference Solution:\n");
@@ -81,12 +50,12 @@ bool DwBranchNonant::chooseBranchingObjects(
 #endif
 
 	double maxdev = 0.0;
-	for (int j = 0; j < tss->getNumCols(0); ++j) {
+	for (int j = 0; j < tss_->getNumCols(0); ++j) {
 		if (devsol[j] > epsilon_) {
 			maxdev = devsol[j];
 			branchingIndex = j;
 			branchingValue = refsol[j];
-			if (tss->getCtypeCore(0)[j] == 'C') {
+			if (tss_->getCtypeCore(0)[j] == 'C') {
 				branchingDownValue = refsol[j] - epsilon_;
 				branchingUpValue = refsol[j] + epsilon_;
 			} else {
@@ -101,32 +70,32 @@ bool DwBranchNonant::chooseBranchingObjects(
 		branched = true;
 
 		message->print(2, "Creating branch objects on column %d (value %e): [%e,%e] and [%e,%e]\n", 
-			branchingIndex, branchingValue, master->clbd_node_[branchingIndex], branchingDownValue, branchingUpValue, master->cubd_node_[branchingIndex]);
+			branchingIndex, branchingValue, master_->clbd_node_[branchingIndex], branchingDownValue, branchingUpValue, master_->cubd_node_[branchingIndex]);
 
 		/** creating branching objects */
 		branchingUp = new DspBranchObj();
 		branchingDn = new DspBranchObj();
-		for (int j = 0; j < ncols_first_stage; ++j) {
-			if (branchingIndex == j % tss->getNumCols(0)) {
+		for (int j = 0; j < tss_->getNumCols(0) * tss_->getNumScenarios(); ++j) {
+			if (branchingIndex == j % tss_->getNumCols(0)) {
 				DSPdebugMessage("Creating branch objects on column %d (value %e): [%e,%e] and [%e,%e]\n", 
-					j, branchingValue, branchingUpValue, master->cubd_node_[j], master->clbd_node_[j], branchingDownValue);
-				branchingUp->push_back(j, branchingUpValue, master->cubd_node_[j]);
-				branchingDn->push_back(j, master->clbd_node_[j], branchingDownValue);
-			} else if (master->clbd_node_[j] > master->clbd_orig_[j] || master->cubd_node_[j] < master->cubd_orig_[j]) {
+					j, branchingValue, branchingUpValue, master_->cubd_node_[j], master_->clbd_node_[j], branchingDownValue);
+				branchingUp->push_back(j, branchingUpValue, master_->cubd_node_[j]);
+				branchingDn->push_back(j, master_->clbd_node_[j], branchingDownValue);
+			} else if (master_->clbd_node_[j] > master_->clbd_orig_[j] || master_->cubd_node_[j] < master_->cubd_orig_[j]) {
 				/** store any bound changes made in parent nodes */
-				DSPdebugMessage("Adjusting bound change on column %d: [%e,%e]\n", j, master->clbd_node_[j], master->cubd_node_[j]);
-				branchingUp->push_back(j, master->clbd_node_[j], master->cubd_node_[j]);
-				branchingDn->push_back(j, master->clbd_node_[j], master->cubd_node_[j]);
+				DSPdebugMessage("Adjusting bound change on column %d: [%e,%e]\n", j, master_->clbd_node_[j], master_->cubd_node_[j]);
+				branchingUp->push_back(j, master_->clbd_node_[j], master_->cubd_node_[j]);
+				branchingDn->push_back(j, master_->clbd_node_[j], master_->cubd_node_[j]);
 			}
 		}
 
 		/** set best dual bounds */
-		branchingUp->bestBound_ = master->getBestDualObjective();
-		branchingDn->bestBound_ = master->getBestDualObjective();
+		branchingUp->bestBound_ = master_->getBestDualObjective();
+		branchingDn->bestBound_ = master_->getBestDualObjective();
 
 		/** assign best dual solutions */
-		branchingUp->dualsol_.assign(master->getBestDualSolution(), master->getBestDualSolution() + master->nrows_);
-		branchingDn->dualsol_.assign(master->getBestDualSolution(), master->getBestDualSolution() + master->nrows_);
+		branchingUp->dualsol_.assign(master_->getBestDualSolution(), master_->getBestDualSolution() + master_->nrows_);
+		branchingDn->dualsol_.assign(master_->getBestDualSolution(), master_->getBestDualSolution() + master_->nrows_);
 
 		/** set branching directions */
 		branchingUp->direction_ = 1;
@@ -142,4 +111,32 @@ bool DwBranchNonant::chooseBranchingObjects(
 	END_TRY_CATCH_RTN(;,false)
 
 	return branched;
+}
+
+void DwBranchNonant::getRefSol(std::vector<double>& refsol) {
+	refsol.resize(tss_->getNumCols(0), 0.0);
+	for (unsigned s = 0; s < master_->getLastSubprobSolutions().size(); ++s) {
+		const CoinPackedVector* sol = master_->getLastSubprobSolutions()[s];
+		int sNumElements = sol->getNumElements();
+		const int* sIndices = sol->getIndices();
+		const double* sElements = sol->getElements();
+		for (int j = 0; j < sNumElements; ++j)
+			if (sIndices[j] < tss_->getNumCols(0) * tss_->getNumScenarios())
+				refsol[sIndices[j] % tss_->getNumCols(0)] += sElements[j] * tss_->getProbability()[s];
+	}
+}
+
+void DwBranchNonant::getDevSol(std::vector<double>& refsol, std::vector<double>& devsol) {
+	devsol.resize(tss_->getNumCols(0), 0.0);
+	for (unsigned s = 0; s < master_->getLastSubprobSolutions().size(); ++s) {
+		const CoinPackedVector* sol = master_->getLastSubprobSolutions()[s];
+		int sNumElements = sol->getNumElements();
+		const int* sIndices = sol->getIndices();
+		const double* sElements = sol->getElements();
+		for (int j = 0; j < sNumElements; ++j)
+			if (sIndices[j] < tss_->getNumCols(0) * tss_->getNumScenarios()) {
+				int k = sIndices[j] % tss_->getNumCols(0);
+				devsol[k] += pow(sElements[j] - refsol[k],2) * tss_->getProbability()[s];
+			}
+	}
 }
--- src/Solver/DantzigWolfe/DwBranchNonant.h
+++ src/Solver/DantzigWolfe/DwBranchNonant.h
@@ -11,26 +11,38 @@
 #define SRC_SOLVER_DANTZIGWOLFE_DWBRANCHNONANT_H_
 
 #include <DantzigWolfe/DwBranch.h>
+#include <Model/TssModel.h>
 
 class DwBranchNonant : public DwBranch {
 public:
 	/** default constructor */
-	DwBranchNonant() : DwBranch() {}
+	DwBranchNonant() : DwBranch(), tss_(NULL), master_(NULL) {}
 
 	/** default constructor with solver */
-	DwBranchNonant(DwModel* model) : DwBranch(model) {}
+	DwBranchNonant(DwModel* model);
 
 	/** default destructor */
 	virtual ~DwBranchNonant() {
+		tss_ = NULL;
+		master_ = NULL;
 	}
 
     virtual bool chooseBranchingObjects(
     			std::vector<DspBranchObj*>& branchingObjs /**< [out] branching objects */);
 
+    /** calculate and return reference solution */
+    virtual void getRefSol(std::vector<double>& refsol);
+
+    /** calculate and return deviation of the reference solution */
+    virtual void getDevSol(std::vector<double>& refsol, std::vector<double>& devsol);
+
 protected:
 
 	/** epsilon value for branching on continuous variables */
 	double epsilon_ = 1.0e-6;
+
+	TssModel* tss_;
+	DwMaster* master_;
 };
 
 #endif /* SRC_SOLVER_DANTZIGWOLFE_DWBRANCHNONANT_H_ */
