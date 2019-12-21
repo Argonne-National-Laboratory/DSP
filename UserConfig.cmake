af9e672e330657aa98cbcd046de98a21e856bdf7
Kibaek Kim <kimk@anl.gov>
2019-12-21 01:05:30 -0600

    updated UserConfig.cmake

--- UserConfig.cmake
+++ UserConfig.cmake
@@ -1,15 +1,10 @@
 # This file defines the user-specific settings.
 
-# Setting CPLEX paths is required.
-# The others (ma27 and scip) are optional. For example, OOQP will be
-# disabled if MA27LIB_DIR is not set.
-
-set(MA27LIB_DIR   "")
-set(CPLEX_LIB_DIR "")
-set(CPLEX_INC_DIR "")
-set(SCIP_DIR      "")
-set(SCIP_LIB_DIR  "")
-set(SPX_DIR       "")
+set(MA27LIB_DIR     "")
+set(CPLEX_LIB_DIR   "")
+set(CPLEX_INC_DIR   "")
+set(SCIPOPT_INC_DIR "")
+set(SCIPOPT_LIB_DIR "")
 
 # Please change OFF to ON once the settings are provided.
 set(USER_SETTINGS OFF)
@@ -17,4 +12,3 @@ set(USER_SETTINGS OFF)
 if(NOT ${USER_SETTINGS})
 	message(FATAL_ERROR "Please complete the user-specific settings in UserConfig.cmake")
 endif()
-set(DEPEND_DIR    $ENV{PWD})
