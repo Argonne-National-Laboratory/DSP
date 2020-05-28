320c8e7a9ada68729ef28ecc9ac97c5ca5390dab
YZhang <39146918+cheesecakeball@users.noreply.github.com>
2020-05-27 22:05:14 -0500

    catch errors

--- src/SolverInterface/DspOsiGrb.h
+++ src/SolverInterface/DspOsiGrb.h
@@ -14,6 +14,25 @@
 #include "OsiGrbSolverInterface.hpp"
 #include "SolverInterface/DspOsi.h"
 
+#define debugMessage \
+  if (false)         \
+  printf
+
+#define GUROBI_CALL(m, x)                                                            \
+  do {                                                                               \
+    int _retcode;                                                                    \
+    if ((_retcode = (x)) != 0) {                                                     \
+      char s[1001];                                                                  \
+      if (grb_->getEnvironmentPtr()) {                                       \
+        sprintf(s, "Error <%d> from GUROBI function call: ", _retcode);              \
+        strncat(s, GRBgeterrormsg(grb_->getEnvironmentPtr()), 1000);         \
+      } else                                                                         \
+        sprintf(s, "Error <%d> from GUROBI function call (no license?).", _retcode); \
+      debugMessage("%s:%d: %s", __FILE__, __LINE__, s);                              \
+      throw CoinError(s, m, "OsiGrbSolverInterface", __FILE__, __LINE__);            \
+    }                                                                                \
+  } while (false)
+
 class DspOsiGrb : public DspOsi {
 public:
 
@@ -41,43 +60,67 @@ public:
 
 	/** load quadratic objective */
 	virtual void loadQuadraticObjective(const CoinPackedMatrix &mat) {
-		if (mat.isColOrdered()) {
-			for (int j = 0; j < mat.getMajorDim(); ++j) {
-				for (int k = 0; k < mat.getVectorSize(j); ++k) {
-					int i = mat.getIndices()[mat.getVectorStarts()[j] + k];
-					double v = mat.getElements()[mat.getVectorStarts()[j] + k];
-                    GUROBI_CALL("loadQuadraticObjective", GRBupdatemodel(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL)));
-                    GUROBI_CALL("loadQuadraticObjective", GRBaddqpterms(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL),
-                    1, i, j, v));
+		try{
+			if (mat.isColOrdered()) {
+				for (int j = 0; j < mat.getMajorDim(); ++j) {
+					for (int k = 0; k < mat.getVectorSize(j); ++k) {
+						int i = mat.getIndices()[mat.getVectorStarts()[j] + k];
+						double v = mat.getElements()[mat.getVectorStarts()[j] + k];
+						int row[1]={i};
+						int col[1]={j};
+						double element[1]={v};
+                    	GUROBI_CALL("loadQuadraticObjective", GRBupdatemodel(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL)));
+                    	GUROBI_CALL("loadQuadraticObjective", GRBaddqpterms(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL),
+                    	1, row, col, element));
+					}
 				}
-			}
-		} else {
-			for (int i = 0; i < mat.getMajorDim(); ++i) {
-				for (int k = 0; k < mat.getVectorSize(i); ++k) {
-					int j = mat.getIndices()[mat.getVectorStarts()[i] + k];
-					double v = mat.getElements()[mat.getVectorStarts()[i] + k];
-					GUROBI_CALL("loadQuadraticObjective", GRBupdatemodel(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL)));
-                    GUROBI_CALL("loadQuadraticObjective", GRBaddqpterms(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL),
-                    1, i, j, v));
+			} else {
+				for (int i = 0; i < mat.getMajorDim(); ++i) {
+					for (int k = 0; k < mat.getVectorSize(i); ++k) {
+						int j = mat.getIndices()[mat.getVectorStarts()[i] + k];
+						double v = mat.getElements()[mat.getVectorStarts()[i] + k];
+						int row[1]={i};
+						int col[1]={j};
+						double element[1]={v};
+						GUROBI_CALL("loadQuadraticObjective", GRBupdatemodel(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL)));
+                    	GUROBI_CALL("loadQuadraticObjective", GRBaddqpterms(grb_->getLpPtr(OsiGrbSolverInterface::KEEPCACHED_ALL),
+                    	1, row, col, element));
+					}
 				}
 			}
 		}
+		catch(const CoinError& e){
+        	e.print();
+		}
     }
+    
 
 	/** solve problem */
 	virtual void solve() {
-		if (si_->getNumIntegers() > 0)
-			si_->branchAndBound();
-		else
-			si_->initialSolve();
-	}
+		try{
+			if (si_->getNumIntegers() > 0)
+				si_->branchAndBound();
+			else
+				si_->initialSolve();
+		}
+		catch(const CoinError& e){
+        	e.print();
+		}
+    }
+	
 
 	/** solution statue */
 	virtual int status() {
+		
 		int status = DSP_STAT_UNKNOWN;
 		int stat;
+		try{
         GUROBI_CALL("status", GRBupdatemodel(grb_->getLpPtr()));
-        GUROBI_CALL("status", GRBgetintattr(grb->getLpPtr(), "Status", &stat));
+        GUROBI_CALL("status", GRBgetintattr(grb_->getLpPtr(), "Status", &stat));
+		}
+		catch(const CoinError& e){
+        	e.print();
+		}
 
         switch(stat) {
         case GRB_OPTIMAL:
@@ -106,58 +149,91 @@ public:
         case GRB_TIME_LIMIT:
             status = DSP_STAT_STOPPED_TIME;
             break;
-        case GRB_NUMERIC:
+
         case GRB_INTERRUPTED:
             status = DSP_STAT_STOPPED_USER;
             break;
         case GRB_LOADED:
         case GRB_INPROGRESS:
+        case GRB_NUMERIC:
             status = DSP_STAT_ABORT;
             break;
 	    default:
             status = DSP_STAT_UNKNOWN;
             break;
+		}
+	
         return status;
 	}
 
 	/** get dual objective value */
 	virtual double getDualObjValue() {
-		double val;
-        GUROBI_CALL("getDualObjVal", GRBupdatemodel(grb_->getLpPtr()));
-		GUROBI_CALL("getDualObjVal", GRBgetdblattr(grb_->getLpPtr(), GRB_DBL_ATTR_OBJBOUND, &val));
-		return val * grb_->getObjSense();
+		try{
+			double val;
+        	GUROBI_CALL("getDualObjVal", GRBupdatemodel(grb_->getLpPtr()));
+			GUROBI_CALL("getDualObjVal", GRBgetdblattr(grb_->getLpPtr(), GRB_DBL_ATTR_OBJBOUND, &val));
+			return val * grb_->getObjSense();
+		}
+		catch(const CoinError& e){
+        	e.print();
+    	}
 	}
 
 	/** get number of branch-and-bound nodes explored */
 	virtual int getNumNodes() {
-		double node;
-        GUROBI_CALL("getNumNodes", GRBupdatemodel(grb_->getLpPtr()));
-        GUROBI_CALL("getNumNodes", GRBgetdblattr(grb->getLpPtr(), GRB_DBL_ATTR_NODECOUNT, &node));
-        return (int)node;
+		try{
+			double node;
+        	GUROBI_CALL("getNumNodes", GRBupdatemodel(grb_->getLpPtr()));
+        	GUROBI_CALL("getNumNodes", GRBgetdblattr(grb_->getLpPtr(), GRB_DBL_ATTR_NODECOUNT, &node));
+        	return (int)node;
+		}
+		catch(const CoinError& e){
+        	e.print();
+    	}
 	}
 
 	/** set number of cores */
 	virtual void setNumCores(int num) {
-        GUROBI_CALL("setNumCores", GRBupdatemodel(grb_->getLpPtr()));
-		GUROBI_CALL("setNumCores", GRBsetintparam(grb->getEnvironmentPtr(), GRB_INT_PAR_THREADS, num));
+		try{
+        	GUROBI_CALL("setNumCores", GRBupdatemodel(grb_->getLpPtr()));
+			GUROBI_CALL("setNumCores", GRBsetintparam(grb_->getEnvironmentPtr(), GRB_INT_PAR_THREADS, num));
+		}
+		catch(const CoinError& e){
+        	e.print();
+    	}
 	}
 
 	/** set time limit */
 	virtual void setTimeLimit(double time) {
-        GUROBI_CALL("setTimeLimit", GRBupdatemodel(grb_->getLpPtr()));
-		GUROBI_CALL("setTimeLimit", GRBsetdblparam(grb_->getEnvironmentPtr(), GRB_DBL_PAR_TIMELIMIT, time));
+		try{
+        	GUROBI_CALL("setTimeLimit", GRBupdatemodel(grb_->getLpPtr()));
+			GUROBI_CALL("setTimeLimit", GRBsetdblparam(grb_->getEnvironmentPtr(), GRB_DBL_PAR_TIMELIMIT, time));
+		}
+		catch(const CoinError& e){
+        	e.print();
+    	}
 	}
 
 	/** set node limit */
 	virtual void setNodeLimit(double num) {
-        GUROBI_CALL("setNodeLimit", GRBupdatemodel(grb_->getLpPtr()));
-		GUROBI_CALL("setNodeLimit", GRBsetdblparam(grb_->getEnvironmentPtr(), GRB_DBL_PAR_NODELIMIT, num));
+		try{
+        	GUROBI_CALL("setNodeLimit", GRBupdatemodel(grb_->getLpPtr()));
+			GUROBI_CALL("setNodeLimit", GRBsetdblparam(grb_->getEnvironmentPtr(), GRB_DBL_PAR_NODELIMIT, num));
+		}
+		catch(const CoinError& e){
+        	e.print();
+    	}
 	}
 
 	/** set relative MIP gap */
 	virtual void setRelMipGap(double tol) {
-        GUROBI_CALL("setRelMipGap", GRBupdatemodel(grb_->getLpPtr()));
-		GUROBI_CALL("setRelMipGap", GRBsetdblparam(grb_->getEnvironmentPtr(), GRB_DBL_PAR_MIPGAP, tol));
+		try{
+        	GUROBI_CALL("setRelMipGap", GRBupdatemodel(grb_->getLpPtr()));
+			GUROBI_CALL("setRelMipGap", GRBsetdblparam(grb_->getEnvironmentPtr(), GRB_DBL_PAR_MIPGAP, tol));
+		}
+		catch(const CoinError& e){
+        	e.print();
+    	}
 	}
 
     OsiGrbSolverInterface* grb_;   
