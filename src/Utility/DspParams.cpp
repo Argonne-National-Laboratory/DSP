9a181773c217c0383c0d92f2aca34f0c2d9139f6
YZhang <39146918+cheesecakeball@users.noreply.github.com>
2020-06-10 23:14:27 -0500

    add Gurobi parameter setting

--- src/Utility/DspParams.cpp
+++ src/Utility/DspParams.cpp
@@ -223,14 +223,14 @@ void DspParams::initIntParams()
 	IntParams_.createParam("DW/MASTER/SOLVER", OsiCpx);
 	IntParams_.createParam("DW/SUB/SOLVER", OsiCpx);
 #else
-	IntParams_.createParam("DW/MASTER/SOLVER", OsiClp);
 #ifdef DSP_HAS_GRB
 	IntParams_.createParam("DE/SOLVER", OsiGrb);
 	IntParams_.createParam("DD/MASTER/SOLVER", OsiGrb);
 	IntParams_.createParam("DD/SUB/SOLVER", OsiGrb);
-	//IntParams_.createParam("DW/MASTER/SOLVER", OsiGrb);
+	IntParams_.createParam("DW/MASTER/SOLVER", OsiGrb);
 	IntParams_.createParam("DW/SUB/SOLVER", OsiGrb);
 #else
+	IntParams_.createParam("DW/MASTER/SOLVER", OsiClp);
 	IntParams_.createParam("BD/SUB/SOLVER", OsiClp);
 	IntParams_.createParam("DD/MASTER/SOLVER", OsiClp);
 #ifdef DSP_HAS_SCIP
