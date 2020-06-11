46fa56e1f3d7b08bedf2ffcbd029d51182fe09dd
YZhang <39146918+cheesecakeball@users.noreply.github.com>
2020-06-10 23:11:25 -0500

    add Gurobi user settings

--- UserConfig.cmake
+++ UserConfig.cmake
@@ -4,7 +4,5 @@ set(CPLEX_LIB_DIR	"")
 set(CPLEX_INC_DIR	"")
 set(GUROBI_LIB_DIR  "")
 set(GUROBI_INC_DIR  "")
-#enter the number defined in gurobi library: for example if libgurobi90.dylib, then enter 90
-set(GUROBI_VERSION  "") 
 set(SCIPOPT_INC_DIR "")
 set(SCIPOPT_LIB_DIR "")
