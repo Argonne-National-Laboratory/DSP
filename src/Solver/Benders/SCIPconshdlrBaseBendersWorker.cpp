aee4350ca78c55184f5c79347a9fbf9f0d2d814f
Kibaek Kim <kibaek.k@gmail.com>
2020-10-13 13:57:39 -0500

    resolved #128

--- src/Solver/Benders/SCIPconshdlrBaseBendersWorker.cpp
+++ src/Solver/Benders/SCIPconshdlrBaseBendersWorker.cpp
@@ -11,7 +11,14 @@
 #include "Solver/Benders/BdMW.h"
 
 SCIPconshdlrBaseBendersWorker::SCIPconshdlrBaseBendersWorker(MPI_Comm comm)
-	: comm_(comm)
+	: comm_(comm),
+	  nsubprobs_(-1),
+	  recvcounts_(NULL),
+	  displs_(NULL),
+	  cut_index_(NULL),
+	  cut_status_(NULL),
+	  cutval_(NULL),
+	  cutrhs_(NULL)
 {
 	MPI_Comm_size(comm_, &comm_size_);
 	MPI_Comm_rank(comm_, &comm_rank_);
@@ -23,14 +30,21 @@ SCIPconshdlrBaseBendersWorker::~SCIPconshdlrBaseBendersWorker()
 	FREE_ARRAY_PTR(displs_);
 	FREE_ARRAY_PTR(cut_index_);
 	FREE_ARRAY_PTR(cut_status_);
+	FREE_2D_ARRAY_PTR(nsubprobs_, cutval_);
+	FREE_ARRAY_PTR(cutrhs_);
 }
 
 void SCIPconshdlrBaseBendersWorker::initialize(int nsubprobs)
 {
+	nsubprobs_ = nsubprobs;
 	recvcounts_ = new int[comm_size_];
 	displs_ = new int[comm_size_];
-	cut_index_ = new int[nsubprobs];
-	cut_status_ = new int[nsubprobs];
+	cut_index_ = new int[nsubprobs_];
+	cut_status_ = new int[nsubprobs_];
+	cutval_ = new double *[nsubprobs_];
+	for (int s = 0; s < nsubprobs_; ++s)
+		cutval_[s] = NULL;
+	cutrhs_ = new double[nsubprobs_];
 
 	/** The master does not solve subproblem. */
 	recvcounts_[0] = 0;
@@ -41,7 +55,7 @@ void SCIPconshdlrBaseBendersWorker::initialize(int nsubprobs)
 	for (int i = 1; i < comm_size_; ++i)
 	{
 		recvcounts_[i] = 0;
-		for (int s = i - 1; s < nsubprobs; s += comm_size_ - 1)
+		for (int s = i - 1; s < nsubprobs_; s += comm_size_ - 1)
 		{
 			recvcounts_[i]++;
 			cut_index_[j++] = s;
@@ -49,7 +63,7 @@ void SCIPconshdlrBaseBendersWorker::initialize(int nsubprobs)
 		displs_[i] = i == 0 ? 0 : displs_[i - 1] + recvcounts_[i - 1];
 		DSPdebugMessage("recvcounts_[%d] %d displs_[%d] %d\n", i, recvcounts_[i], i, displs_[i]);
 	}
-	DSPdebug(DspMessage::printArray(nsubprobs, cut_index_));
+	DSPdebug(DspMessage::printArray(nsubprobs_, cut_index_));
 }
 
 void SCIPconshdlrBaseBendersWorker::generateCutsBase(
@@ -60,13 +74,7 @@ void SCIPconshdlrBaseBendersWorker::generateCutsBase(
 	const double *probability, /**< [in] probability */
 	OsiCuts *cuts /**< [out] cuts generated */)
 {
-#define FREE_MEMORY                      \
-	FREE_2D_ARRAY_PTR(nsubprobs, cutval) \
-	FREE_ARRAY_PTR(cutrhs)
-
 	OsiCuts cuts_collected;
-	double **cutval = NULL; /** dense cut coefficients for each subproblem */
-	double *cutrhs = NULL;	/** cut rhs for each subproblem */
 
 	BGN_TRY_CATCH
 
@@ -86,26 +94,20 @@ void SCIPconshdlrBaseBendersWorker::generateCutsBase(
 	MPIgatherOsiCuts(comm_, *cuts, cuts_collected);
 	DSPdebugMessage("[%d]: Collected %d cuts\n", comm_rank_, cuts_collected.sizeCuts());
 
-	/** allocate memory */
-	/** TODO: use memeber variables to avoid reallocation */
-	cutval = new double *[nsubprobs];
-	cutrhs = new double[nsubprobs];
-
 	for (int i = 0; i < cuts_collected.sizeCuts(); ++i)
 	{
+		FREE_ARRAY_PTR(cutval_[i]);
 		OsiRowCut *rc = cuts_collected.rowCutPtr(i);
-		cutval[i] = rc->row().denseVector(nvars);
-		cutrhs[i] = rc->lb();
+		cutval_[i] = rc->row().denseVector(nvars);
+		cutrhs_[i] = rc->lb();
 	}
 
 	/** aggregate cuts */
-	aggregateCutsBase(nsubprobs, nvars, naux, cutval, cutrhs, probability, cuts);
+	aggregateCutsBase(nsubprobs, nvars, naux, probability, cuts);
 	DSPdebug(cuts->printCuts());
 	// cuts->printCuts();
 
-	END_TRY_CATCH(FREE_MEMORY)
-
-	FREE_MEMORY
+	END_TRY_CATCH(;)
 
 #undef FREE_MEMORY
 }
@@ -114,8 +116,6 @@ void SCIPconshdlrBaseBendersWorker::aggregateCutsBase(
 	int nsubprobs,			   /**< [in] number of subproblems */
 	int nvars,				   /**< [in] number of variables */
 	int naux,				   /**< [in] number of auxiliary variables */
-	double **cutvec,		   /**< [in] cut vector */
-	double *cutrhs,			   /**< [in] cut right-hand side */
 	const double *probability, /**< [in] probability */
 	OsiCuts *cuts /**< [out] cuts generated */)
 {
@@ -147,13 +147,13 @@ void SCIPconshdlrBaseBendersWorker::aggregateCutsBase(
 		{
 			/** set cut body */
 			for (int j = 0; j < nvars; ++j)
-				if (fabs(cutvec[i][j]) > 1.0e-8)
-					vec.insert(j, cutvec[i][j]);
+				if (fabs(cutval_[i][j]) > 1.0e-8)
+					vec.insert(j, cutval_[i][j]);
 
 			OsiRowCut fcut;
 			fcut.setRow(vec);
 			fcut.setUb(COIN_DBL_MAX);
-			fcut.setLb(cutrhs[i]);
+			fcut.setLb(cutrhs_[i]);
 
 			cuts->insert(fcut);
 			isInfeasible = true;
@@ -178,8 +178,8 @@ void SCIPconshdlrBaseBendersWorker::aggregateCutsBase(
 
 		/** calculate weighted aggregation of cuts */
 		for (int j = 0; j < nvars; ++j)
-			aggval[ind_aux][j] += cutvec[i][j] * probability[cut_index_[i]];
-		aggrhs[ind_aux] += cutrhs[i] * probability[cut_index_[i]];
+			aggval[ind_aux][j] += cutval_[i][j] * probability[cut_index_[i]];
+		aggrhs[ind_aux] += cutrhs_[i] * probability[cut_index_[i]];
 	}
 
 	/** We generate optimality cuts only if there is no feasibility cut generated. */
