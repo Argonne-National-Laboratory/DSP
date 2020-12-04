1071ef1726134e6f7d046e2d7349a3c09be7e097
Kibaek Kim <kibaek.k@gmail.com>
2020-12-04 10:41:11 -0600

    Update UserConfig.cmake

--- UserConfig.cmake
+++ UserConfig.cmake
@@ -1,7 +1,7 @@
 # This file defines the user-specific settings.
 set(MA27LIB_DIR     "")
-set(CPLEX_LIB_DIR   "/Applications/CPLEX_Studio1210/cplex/lib/x86-64_osx/static_pic")
-set(CPLEX_INC_DIR   "/Applications/CPLEX_Studio1210/cplex/include/ilcplex")
+set(CPLEX_LIB_DIR   "")
+set(CPLEX_INC_DIR   "")
 set(GUROBI_LIB_DIR  "")
 set(GUROBI_INC_DIR  "")
 set(SCIPOPT_INC_DIR "")
