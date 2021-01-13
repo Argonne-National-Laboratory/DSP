f37e037c5d0649c774a224ac1b3aa0a99549ae5c
Kibaek Kim <kibaek.k@gmail.com>
2021-01-13 13:05:27 -0600

    Fixed #199

--- src/SolverInterface/DspOsiCpx.h
+++ src/SolverInterface/DspOsiCpx.h
@@ -519,22 +519,23 @@ public:
 
 	/** set number of cores */
 	virtual void setNumCores(int num) {
-		CPXsetintparam(cpx_->getEnvironmentPtr(), CPX_PARAM_THREADS, num);
+		CPXsetintparam(cpx_->getEnvironmentPtr(), CPX_PARAM_THREADS, CoinMax(0, num));
 	}
 
 	/** set time limit */
 	virtual void setTimeLimit(double time) {
-		CPXsetdblparam(cpx_->getEnvironmentPtr(), CPX_PARAM_TILIM, time);
+		CPXsetdblparam(cpx_->getEnvironmentPtr(), CPX_PARAM_TILIM, CoinMax(CoinMin(1.0e+75, time), 1.0e-4));
 	}
 
 	/** set node limit */
-	virtual void setNodeLimit(double num) {
-		CPXsetintparam(cpx_->getEnvironmentPtr(), CPX_PARAM_NODELIM, num);
+	virtual void setNodeLimit(int num)
+	{
+		CPXsetintparam(cpx_->getEnvironmentPtr(), CPX_PARAM_NODELIM, CoinMax(1, CoinMin(2100000000, num)));
 	}
 
 	/** set relative MIP gap */
 	virtual void setRelMipGap(double tol) {
-		CPXsetdblparam(cpx_->getEnvironmentPtr(), CPX_PARAM_EPGAP, tol);
+		CPXsetdblparam(cpx_->getEnvironmentPtr(), CPX_PARAM_EPGAP, CoinMax(0.0, CoinMin(1.0, tol)));
 	}
 
     OsiCpxSolverInterface* cpx_;   
