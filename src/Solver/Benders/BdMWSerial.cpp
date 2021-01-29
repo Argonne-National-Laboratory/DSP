bcca43d825e744d164c43ae4de094a753a9e0e90
YZhang <39146918+cheesecakeball@users.noreply.github.com>
2021-01-29 08:12:56 -0500

    set Benders Callback function for other solvers

--- src/Solver/Benders/BdMWSerial.cpp
+++ src/Solver/Benders/BdMWSerial.cpp
@@ -5,7 +5,7 @@
  *      Author: kibaekkim
  */
 
-// #define DSP_DEBUG
+#define DSP_DEBUG
 
 #include "Solver/Benders/BdMWSerial.h"
 #include "Solver/Benders/SCIPconshdlrBenders.h"
@@ -56,8 +56,15 @@ DSP_RTN_CODE BdMWSerial::run()
 	BGN_TRY_CATCH
 
 	/** set constraint handler */
-	DSPdebugMessage("setConshdlr\n");
-	master_->setConshdlr(constraintHandler());
+	
+	if (par_->getIntParam("BD/MASTER/SOLVER")==OsiScip){
+		DSPdebugMessage("setConshdlr for SCIP\n");
+		master_->setConshdlr(constraintHandler());
+	}
+	else if (par_->getIntParam("BD/MASTER/SOLVER")==OsiGrb){
+		master_->setBendersCallback(BendersCallbackFunc());
+	}
+	
 
 	/** solve */
 	DSPdebugMessage("solve\n");
@@ -126,4 +133,49 @@ SCIPconshdlrBenders* BdMWSerial::constraintHandler()
 	END_TRY_CATCH_RTN(;, NULL)
 
 	return conshdlr;
+}
+
+DSP_RTN_CODE BdMWSerial::BendersCallbackFunc(){
+
+	BGN_TRY_CATCH
+
+	BendersCallback * Bdcb = NULL;
+
+	BGN_TRY_CATCH
+
+	int naux = par_->getIntParam("BD/NUM_CUTS_PER_ITER");
+	int priority = par_->getIntParam("BD/CUT_PRIORITY");
+	int sepa_solver = par_->getIntParam("BD/SUB/SOLVER");
+
+	/** get solver interface */
+	DspOsi * osi = dynamic_cast<DspOsi*>(master_->getDspOsiPtr());
+
+	/** get Benders subproblem solver */
+	BdSub *bdsub = worker_->getBdSubPtr();
+
+	/** check whether integer Benders cuts need used or not */
+	bool add_integer_benders = false;
+	if (model_->isStochastic() && master_->is_binary())
+		add_integer_benders = true;
+
+	/** Benders constraint handler 
+	 * TODO: add integer Benders cuts
+	 * TODO: if Dro
+	*/
+	
+	if (model_->isDro())
+		printf("To implement Benders Callback for Dro...\n");
+	else{
+		Bdcb = new BendersCallback(osi, "Benders", priority);
+	}
+	
+	Bdcb->setDecModel(model_);
+	Bdcb->setBdSub(bdsub);
+	Bdcb->setOriginalVariables(osi->si_->getNumCols(), naux);
+
+	END_TRY_CATCH_RTN(;, NULL)
+
+	return conshdlr;
+
+	return DSP_RTN_OK
 }
