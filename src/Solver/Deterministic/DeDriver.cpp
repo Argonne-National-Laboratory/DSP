c424df330cd7791ce0d34dc637a8d5ed04e26676
Kibaek Kim <kibaek.k@gmail.com>
2021-12-16 15:26:10 -0600

    fixed freeing memory

--- src/Solver/Deterministic/DeDriver.cpp
+++ src/Solver/Deterministic/DeDriver.cpp
@@ -52,7 +52,8 @@ DSP_RTN_CODE DeDriver::run()
 	FREE_PTR(qobj)              \
 	FREE_ARRAY_PTR(rlbd)        \
 	FREE_ARRAY_PTR(rubd)        \
-	FREE_ARRAY_PTR(qc_row_scen) \
+	FREE_PTR(qc_row_core)       \
+	FREE_2D_PTR(nscen, qc_row_scen) \
 	FREE_ARRAY_PTR(linind)      \
 	FREE_ARRAY_PTR(quadrow)     \
 	FREE_ARRAY_PTR(quadcol)
