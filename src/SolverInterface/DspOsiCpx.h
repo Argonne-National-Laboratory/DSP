2fe2479ce4dd40a9fb7121c0408df625e9fde418
Kibaek Kim <kibaek.k@gmail.com>
2022-08-04 10:49:16 -0500

    temporary switch from barrier to simplex for numerical issue

--- src/SolverInterface/DspOsiCpx.h
+++ src/SolverInterface/DspOsiCpx.h
@@ -78,7 +78,10 @@ public:
 		CPXsetintparam(cpx_->getEnvironmentPtr(), CPX_PARAM_LPMETHOD, CPX_ALG_BARRIER);
 		// CPXsetintparam(cpx_->getEnvironmentPtr(), CPX_PARAM_BARCROSSALG, -1); // This has been deprecated.
 		CPXsetintparam(cpx_->getEnvironmentPtr(), CPXPARAM_SolutionType, CPX_NONBASIC_SOLN);
-		CPXsetdblparam(cpx_->getEnvironmentPtr(), CPX_PARAM_BAREPCOMP, 1e-5);
+		// CPXsetdblparam(cpx_->getEnvironmentPtr(), CPXPARAM_Barrier_ConvergeTol, 1e-5);
+		// CPXsetintparam(cpx_->getEnvironmentPtr(), CPXPARAM_Emphasis_Numerical, CPX_ON);
+		// CPXsetintparam(cpx_->getEnvironmentPtr(), CPXPARAM_Preprocessing_Reduce, 1);
+		// CPXsetintparam(cpx_->getEnvironmentPtr(), CPXPARAM_Preprocessing_Presolve, 1);
 	}
 
 	/** solution statue */
