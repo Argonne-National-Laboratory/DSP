17013fe52ac8a4a9b93213d80b29d229ab80d645
Kibaek Kim <kibaek.k@gmail.com>
2022-08-04 11:46:45 -0500

    provide reproducible setting

--- examples/dro/experiments/run_script.sh
+++ examples/dro/experiments/run_script.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=bdwall
 #SBATCH --account=NEXTGENOPT
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=32
-#SBATCH --time=4:00:00
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=2:00:00
 #SBATCH --mail-user=kimk@anl.gov
 #SBATCH --mail-type=END
 
@@ -12,18 +12,25 @@ export I_MPI_FABRICS=shm:tmi
 export OMP_NUM_THREADS=1
 
 DSP=../../../build/bin/runDsp
-K=$1
-NP=64
+INSTANCES=( "233" "243" "332" "342" )
+K=( "20" "50" "100" "200" "300" )
+EPS=( "1" "100" "500" "1000" )
 
-echo "##########"
-echo "# K=${K} #"
-echo "##########"
-echo ""
+for i in "${INSTANCES[@]}"
+do
+    for k in "${K[@]}"
+    do
+        for e in "${EPS[@]}"
+        do
+            $DSP --algo drdd --wassnorm 2.0 --wasseps ${e}.0 --smps drdcap_${i}_10_${k} --param params.txt > outputs/drdcap_${i}_${k}_${e}.txt &
+        done
+    done
+done
 
-srun -n $NP $DSP --algo drdd --wassnorm 2.0 --wasseps 100.0 --smps drdcap_332_10_${K} --param params.txt > outputs/drdcap_332_${K}_100_${NP}.txt
+wait
 
 # Run the following in the command line
-# sbatch -J drdcap_scale_500 -o drdcap_scale_$(date +%Y%m%d%H%M%S).out run_script_scaling.sh 500
+# sbatch -J drdcap_500 -o drdcap_$(date +%Y%m%d%H%M%S).out run_script.sh 500
 
 # Useful
 # squeue -u kimk
